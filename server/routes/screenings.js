9d51bdbe2e8e140075bbcda3e6a2bf3b59fc6963
Gabriela Froń <gabriela.fron04@gmail.com>
2026-07-26 13:10:43 +0200

    feature(database): add screenings for movie

--- server/routes/screenings.js
+++ server/routes/screenings.js
@@ -3,6 +3,8 @@ import { db } from "../db/database.js";
 
 export const screeningsRouter = Router();
 
+
+// get all screenings 
 screeningsRouter.get("/", (request, response) => {
   const screenings = db
     .prepare(
@@ -19,6 +21,30 @@ screeningsRouter.get("/", (request, response) => {
       `,
     )
     .all();
+
+  response.json(screenings);
+});
+
+//get all screening for movie id
+screeningsRouter.get("/:movieId", (request, response) => {
+  const { movieId } = request.params;
+
+  const screenings = db
+    .prepare(
+      `
+      SELECT
+        screenings.id,
+        screenings.screening_date,
+        screenings.screening_time,
+        screenings.room_name,
+        movies.title AS movie_title
+      FROM screenings
+      JOIN movies ON movies.id = screenings.movie_id
+      WHERE movies.id = ?
+      ORDER BY screenings.screening_date, screenings.screening_time
+      `,
+    )
+    .all(movieId);
 
   response.json(screenings);
 });
