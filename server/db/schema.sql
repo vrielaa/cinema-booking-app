1aada7fe57e288344854d88d5e5c5a8e635a41c9
Gabriela Froń <gabriela.fron04@gmail.com>
2026-07-26 13:00:13 +0200

    feature(database): add rooms

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,22 +1,29 @@
 CREATE TABLE IF NOT EXISTS movies (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  id INTEGER PRIMARY KEY,
   title TEXT NOT NULL,
   description TEXT,
   duration_minutes INTEGER NOT NULL,
   poster_path TEXT NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS rooms (
+  id INTEGER PRIMARY KEY,
+  row_count INTEGER NOT NULL,
+  seats_per_row INTEGER NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS screenings (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  id INTEGER PRIMARY KEY,
   movie_id INTEGER NOT NULL,
+  room_id INTEGER NOT NULL,
   screening_date TEXT NOT NULL,
   screening_time TEXT NOT NULL,
-  room_name TEXT NOT NULL,
-  FOREIGN KEY (movie_id) REFERENCES movies(id)
+  FOREIGN KEY (movie_id) REFERENCES movies(id),
+  FOREIGN KEY (room_id) REFERENCES rooms(id)
 );
 
 CREATE TABLE IF NOT EXISTS bookings (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  id INTEGER PRIMARY KEY,
   screening_id INTEGER NOT NULL,
   customer_name TEXT NOT NULL,
   seats TEXT NOT NULL,
